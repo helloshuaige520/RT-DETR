35c2ea2f3781c304a2b0282719f457f559ae0ffa
helloshuaige520 <a296686236@gmail.com>
2026-02-01 13:47:35 +0800

    add experience comparison in caomparison_table.md and add a command.txt

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
                 batch=4, # batchsize 不建议乱动，一般来说4的效果都是最好的，越大的batch效果会很差(经验之谈)
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
                 # device='0,1', # 指定显卡和多卡训练参考<使用教程.md>下方常见错误和解决方案
-                # resume='/home/rtdetr/project/RTDETR-20251122/RTDETR-20251122/RTDETR-main/runs/train/exp-visdrone2019-rtdetr-P2/weights/last.pt', # last.pt path
+                 resume='/home/rtdetr/project/RTDETR-20251122/RTDETR-20251122/RTDETR-main/runs/train/exp-visdrone2019-rtdetr-C2f-Additive-CGLU + P2 Head/weights/last.pt', # last.pt path
                 patience=0, # 设置0代表不早提供，设置30代表精度持续30epoch没有比之前最高的高就早停
                 project='runs/train',
                 name='exp-visdrone2019-rtdetr-C2f-Additive-CGLU + P2 Head',
